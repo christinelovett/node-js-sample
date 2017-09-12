65552ad9ab8a814652e099914b8ba692a69ef0f2
Christine Lovett <christinelovett@Dockers-MacBook-Pro.local>
2017-09-12 11:20:37 -0400

    Editing simple node app content.

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
+app.set('view engine', 'pug')
+
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
 app.listen(app.get('port'), function() {
